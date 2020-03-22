a0fd77dd9f59c05a52faa5a2ee30a97f8d5f2278
DaDiCeMa <zaharalex25@gmail.com>
2020-03-22 18:45:11 +0100

    Finishing table

--- src/containers/Table.js
+++ src/containers/Table.js
@@ -4,27 +4,46 @@ import './Table.css'
 
 
 class Table extends Component {
+    componentDidMount() {
+     // const candidatesData =
+     //   "http://personio-fe-test.herokuapp.com/api/v1/candidates";
+ 
+     axios
+       .get("http://personio-fe-test.herokuapp.com/api/v1/candidates")
+       .then(response => {
+         console.log(response);
+         // const candidates = response.data.slice(0, 4);
+         // const updateAplicants = candidates.map(post => {
+         //   return { ...candidates, email: "something" };
+         // });
+         this.setState({ candidates: response.data.data });
+       });
+   }
    constructor(props) {
       super(props) //since we are extending class Table so we have to use super in order to override Component class constructor
       this.state = { //state is by default an object
          candidates: [
-            { id: 1, name: 'Wasif', age: 21, email: 'wasif@email.com' },
-            { id: 2, name: 'Ali', age: 19, email: 'ali@email.com' },
-            { id: 3, name: 'Saad', age: 16, email: 'saad@email.com' },
-            { id: 4, name: 'Asad', age: 25, email: 'asad@email.com' }
+            { id: 12111, name: 'Wasif', age: 21, email: 'wasif@email.com' },
+            // { id: 2, name: 'Ali', age: 19, email: 'ali@email.com' },
+            // { id: 3, name: 'Saad', age: 16, email: 'saad@email.com' },
+            // { id: 4, name: 'Asad', age: 25, email: 'asad@email.com' }
          ]
       }
    }
 
    renderTableData() {
     return this.state.candidates.map((candidate, index) => {
-       const { id, name, age, email } = candidate //destructuring
+       const { id, name, age, email,birth_date, year_of_experience,position_applied,application_date,status} = candidate //destructuring
        return (
           <tr key={id}>
              <td>{id}</td>
              <td>{name}</td>
-             <td>{age}</td>
              <td>{email}</td>
+             <td>{birth_date}</td>
+             <td>{year_of_experience}</td>
+             <td>{position_applied}</td>
+             <td>{application_date}</td>
+             <td>{status}</td>
           </tr>
        )
     })
