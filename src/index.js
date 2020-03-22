71407deb293c948b576ddb2acd2f08780843d3f9
DaDiCeMa <zaharalex25@gmail.com>
2020-03-22 16:00:16 +0100

    Adding the project skeleton

---
+++ src/index.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { createStore, combineReducers, applyMiddleware, compose } from 'redux';
+import { Provider } from 'react-redux';
+import thunk from 'redux-thunk'
+
+import './index.css';
+import App from './App';
+import registerServiceWorker from './registerServiceWorker';
+import counterReducer from './store/reducers/counter';
+import resultReducer from './store/reducers/result';
+
+const rootReducer = combineReducers({
+    ctr: counterReducer,
+    res:resultReducer
+})
+
+
+// Enabling Redux devtools for this project
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+// Middleware that does Logging into the console when actions dispatch
+
+// const logger = (store) => {
+//     return (next) => {
+//         return (action) => {
+//                 console.log('Middleware dispatching:', action);
+//                 const result = next(action)
+//                 console.log('middleware next state', store.getState())
+//                 return result
+//         }
+//     }
+// };
+
+const store = createStore(rootReducer, composeEnhancers(applyMiddleware( thunk)));
+
+ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('root'));
+registerServiceWorker();
