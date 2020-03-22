0d5d6e554b96ed8d0d19a2b636153b03b2205bdc
DaDiCeMa <zaharalex25@gmail.com>
2020-03-22 16:28:08 +0100

    Creating Reducers and components
    Added 3 reducer types inside the store + components

--- src/index.js
+++ src/index.js
@@ -7,12 +7,14 @@ import thunk from 'redux-thunk'
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
-import counterReducer from './store/reducers/counter';
+import sortReducer from './store/reducers/sort';
+import filterReducer from './store/reducers/filter';
 import resultReducer from './store/reducers/result';
 
 const rootReducer = combineReducers({
-    ctr: counterReducer,
-    res:resultReducer
+    sort: sortReducer,
+    filter: filterReducer,
+    res: resultReducer
 })
 
 
