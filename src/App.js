f2ac24b9dd1375a20908ebe8ff6b895b160f6486
DaDiCeMa <zaharalex25@gmail.com>
2020-03-23 20:49:54 +0100

    created footer

--- src/App.js
+++ src/App.js
@@ -1,15 +1,19 @@
 import React from "react";
-import "./App.css";
-// import Spinner from "./components/Spinner/Spinner";
+import classes from "./App.css";
+import Spinner from "./components/Spinner/Spinner";
 import Navigation from "./components/Navigation/Navigation";
 import CandidateTable from "./containers/CandidateTable/CandidateTable";
+// import Footer from "./components/Footer/Footer";
 
 function App() {
   return (
-    <div className="App">
-      <Navigation />
-      {/* <Spinner></Spinner> */}
-      <CandidateTable />
+    <div>
+      <div className={classes.App}>
+        <Navigation />
+        {/* <Spinner></Spinner> */}
+        <CandidateTable />
+        {/* <Footer /> */}
+      </div>
     </div>
   );
 }
---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,8 @@
+import React from "react";
+import classes from "./Footer.module.css";
+
+const footer = () => (
+  <div className={classes.Footer__wrapper}> Copyright Personio </div>
+);
+
+export default footer;
