69419495a9c7feb1229edc26704aa5dc4e67a448
DaDiCeMa <zaharalex25@gmail.com>
2020-03-24 20:53:26 +0100

    FIX

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
 import React from "react";
 import classes from "./App.css";
-import Spinner from "./components/Spinner/Spinner";
 import Navigation from "./components/Navigation/Navigation";
 import CandidateTable from "./containers/CandidateTable/CandidateTable";
 // import Footer from "./components/Footer/Footer";
 // import Modal from "./components/Modal/Modal";
-import Aux from "./hoc/Aux";
+// import Aux from "./hoc/Aux";
 
 function App() {
   return (
-    <Aux>
+    // <Aux>
       <div className={classes.App}>
         <Navigation />
         {/* <Modal></Modal> */}
-        {/* <Spinner></Spinner> */}
+     
         <CandidateTable />
         {/* <Footer /> */}
       </div>
-    </Aux>
+    // </Aux>
   );
 }
 
