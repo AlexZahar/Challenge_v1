3140bbc5939dbef2a5469a1ad3b5a252de22ea60
DaDiCeMa <zaharalex25@gmail.com>
2020-03-27 19:17:49 +0100

    cat de cat

--- src/containers/CandidateTable/CandidateTable.js
+++ src/containers/CandidateTable/CandidateTable.js
@@ -1,136 +1,94 @@
 import React, { Component } from "react";
 import classes from "./CandidateTable.module.css";
 import Aux from "../../hoc/Aux";
-import {connect} from 'react-redux'
-import Modal from '../../components/Modal/Modal'
+import { connect } from "react-redux";
+import Modal from "../../components/Modal/Modal";
 import axios from "../../axios-config";
 import Spinner from "../../components/Spinner/Spinner";
-import * as actionTypes from '../../store/reducers/actions/actionTypes'
-import withErrorHandler from '../../hoc/withErrorHandler/withErrorHandler'
-import * as actionCreators from '../../store/reducers/actions/actionCreators'
+import * as actionTypes from "../../store/reducers/actions/actionTypes";
+import withErrorHandler from "../../hoc/withErrorHandler/withErrorHandler";
+import * as actionCreators from "../../store/reducers/actions/actionCreators";
 
 class CandidateTable extends Component {
-  
-    // state = {
-    //   //state is by default an object
-    //   candidates: [],
-    //   showTable: false
-    // };
-  
+  constructor(props) {
+    super(props); //since we are extending class Table so we have to use super in order to override Component class constructor
+    this.state = {
+      //state is by default an object
+      candidates: [],
+      showTable: false
+    };
+  }
   componentDidMount() {
-    return this.props.onFetchCandidates()
-    
-    
-   
+    this.props.onFetchCandidates();
   }
 
-  checkUndefined =() => {
-    if(typeof this.props.candidates[0] === 'undefined' ){
-      console.log('pizdaaa')
-    }
+  renderTableData() {
+    return this.props.candidates.map((candidate, index) => {
+      const {
+        id,
+        name,
+        birth_date,
+        year_of_experience,
+        position_applied,
+        application_date,
+        status,
+        email
+      } = candidate; //destructuring
+      return (
+        <tr key={id}>
+          <td>{id}</td>
+          <td>{name}</td>
+          <td>{email}</td>
+          <td>{birth_date}</td>
+          <td>{year_of_experience}</td>
+          <td>{position_applied}</td>
+          <td>{application_date}</td>
+          <td>{status}</td>
+        </tr>
+      );
+    });
   }
-  // onGetCandidatesHandler = async () => {
-  //   try {
-  //     let res = await axios.get(
-  //       "http://personio-fe-test.herokuapp.com/api/v1/candidates"
-  //     );
-  //     console.log(res);
-  //     let { data } = res.data;
-  //     this.setState({ candidates: data });
-  //   } catch (err) {
-  //     console.log(err);
-  //     throw err;
-  //   }
-  // };
 
-  onRenderTableData() {
-    // return this.state.props.candidates.map((candidates, index) => {
-    //   const {
-    //     id,
-    //     name,
-    //     birth_date,
-    //     year_of_experience,
-    //     position_applied,
-    //     application_date,
-    //     status,
-    //     email
-    //   } = candidates; //destructuring
-    //   return (
-    //     <tr key={id}>
-    //       <td>{id}</td>
-    //       <td>{name}</td>
-    //       <td>{email}</td>
-    //       <td>{birth_date}</td>
-    //       <td>{year_of_experience}</td>
-    //       <td>{position_applied}</td>
-    //       <td>{application_date}</td>
-    //       <td>{status}</td>
-    //     </tr>
-    //   );
-    // });
-    console.log('this issss', this.props.tableData)
+  toggleTableHandler = () => {
+    // this.getCandidates();
+    const doesShow = this.state.showTable;
+    this.setState({ showTable: !doesShow });
+  };
+
+  renderTableHeader() {
+    let header = Object.keys(this.props.candidates[0]);
+    return header.map((key, index) => {
+      return <th key={index}>{key.toUpperCase()}</th>;
+    });
   }
-  // onToggleTableHandler = () => {
-  //   this.onGetCandidatesHandler();
-  //   const doesShow = this.state.showTable;
-  //   this.setState({ showTable: !doesShow });
-  // };
-
-  // onRenderTableHeader() {
-  //   let header = Object.keys(this.props.candidates[0]);
-  //   return header.map((key, index) => {
-  //     return <th key={index}>{key.toUpperCase()}</th>;
-  //   });
-  // }
 
   render() {
-    //Whenever our class runs, render method will be called automatically, it may have already defined in the constructor behind the scene.
-    // let spinner = null;
-    // let table = null;
-    // if (this.props.showTable | (this.props.candidates.length > 1)) {
-    //   spinner = null;
-    //   table = (
-    //     <div>
-    //       <h2 className={classes.Title}>Candidates Table</h2>
-    //       <table className={classes.Candidates}>
-    //         <tbody>
-    //           <tr>{this.onRenderTableHeader()}</tr>
-    //           {this.onRenderTableData()}
-    //         </tbody>
-    //       </table>
-    //     </div>
-    //   );
-    // } else if (!this.props.showTable) {
-    //   spinner = <Spinner />;
-    // }
-    if(this.props.tableData){
-      return this.onRenderTableData()
+    let spinner = null;
+    let table = null;
+    if (this.state.showTable | (this.props.candidates.length > 1)) {
+      spinner = null;
+      table = (
+        <div>
+          <h2 className={classes.Title}>Candidates Table</h2>
+          <table className={classes.Candidates}>
+            <tbody>
+              <tr>{this.renderTableHeader()}</tr>
+              {this.renderTableData()}
+            </tbody>
+          </table>
+        </div>
+      );
+    } else if (!this.state.showTable) {
+      spinner = <Spinner />;
     }
-    let spinner = null
-    let button = null
-    if(this.props.loadingCandidates){
-      spinner = <Spinner/>
-    }
-    // if(this.props.candidates.error == 'error'){
-    //   console.log('OMGGGGGG')
-    //   button = (
-    //     <button onClick={this.props.onFetchCandidates}>Refresh</button>
-    //   )
-    // }
-    
-    return (
-   
 
+    return (
       <Aux>
-        {/* <button onClick={this.onInitCandidates()}>Show Candidate Table</button> */}
+        <button onClick={this.toggleTableHandler}>Show Candidate Table</button>
         <div className={classes.Candidates__wrapper}>
+          {table}
           {spinner}
-          {button}
-          {/* {table}
-          {spinner} */}
-          {/* {this.props.candidates} */}
-          <button onClick={this.props.onRenderTableData}>Refresh</button>
-           </div>
+        </div>
       </Aux>
     );
   }
@@ -138,21 +96,22 @@ class CandidateTable extends Component {
 
 const mapStateToProps = state => {
   return {
-candidates: state.candidates,
-loadingCandidates: state.loading,
-tableData: state.tableData
+    candidates: state.candidates,
+    loadingCandidates: state.loading,
+    tableData: state.tableData
   };
-}
+};
 
 const mapDispatchToProps = dispatch => {
   return {
-    
     onFetchCandidates: () => dispatch(actionCreators.fetchCandidates()),
-    onRenderTableData: () => dispatch(actionCreators.onRenderTableData()),
+    onRenderTableData: () => dispatch(actionCreators.onRenderTableData())
     // onRenderTableDataHandler: () => dispatch(actionCreators.onRenderTableDataHandler()),
     // onToggleTableHandler: () => dispatch(actionCreators.onToggleTableHandler())
-  }
-}
+  };
+};
 
-
-export default connect(mapStateToProps,mapDispatchToProps)(withErrorHandler(CandidateTable, axios));
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(withErrorHandler(CandidateTable, axios));
--- src/store/reducers/actions/actionCreators.js
+++ src/store/reducers/actions/actionCreators.js
@@ -1,65 +1,60 @@
-import * as actionTypes from './actionTypes' 
-import axios from '../../../axios-config'
-
+import * as actionTypes from "./actionTypes";
+import axios from "../../../axios-config";
 
 export const renderTableData = () => {
-    return {
-        type: actionTypes.RENDER_TABLE_DATA
-    }
-}
-
-export const onRenderTableData = () => {
-return dispatch => {
-    console.log('pulaaa')
-    
-    dispatch(renderTableData())
-}
-}
-// ----------------------------------------------
-
-export const fetchCandidatesSuccess = ( candidates ) => {
-    return {
-        type: actionTypes.FETCH_CANDIDATES_SUCCESS,
-        candidates: candidates
-    };
+  return {
+    type: actionTypes.RENDER_TABLE_DATA
+  };
 };
 
-export const fetchCandidatesFail = ( error ) => {
-    return {
-        type: actionTypes.FETCH_CANDIDATES_FAILED,
-        error: error
-    };
+export const onRenderTableData = () => {
+  return dispatch => {
+    console.log("pulaaa");
+
+    dispatch(renderTableData());
+  };
+};
+// ----------------------------------------------
+
+export const fetchCandidatesSuccess = candidates => {
+  return {
+    type: actionTypes.FETCH_CANDIDATES_SUCCESS,
+    candidates: candidates
+  };
+};
+
+export const fetchCandidatesFail = error => {
+  return {
+    type: actionTypes.FETCH_CANDIDATES_FAILED,
+    error: error
+  };
 };
 
 export const fetchCandidatesStart = () => {
-    return {
-        type: actionTypes.FETCH_CANDIDATES_START,
-        
-    };
+  return {
+    type: actionTypes.FETCH_CANDIDATES_START
+  };
 };
 
 export const fetchCandidates = () => {
-    return dispatch => {
-        dispatch(fetchCandidatesStart());
-        axios.get( '/candidates' )
-            .then( res => {
-                console.log(res)
-                const fetchedCandidates = []
-                fetchedCandidates.push(res.data.data)
+  return dispatch => {
+    dispatch(fetchCandidatesStart());
+    axios
+      .get("/candidates")
+      .then(res => {
+        console.log("this is the response:", res);
+        const fetchedCandidates = [];
+        fetchedCandidates.push(res.data);
 
-                console.log(fetchedCandidates)
-                dispatch(fetchCandidatesSuccess(fetchedCandidates));
-            } )
-            .catch( err => {
-                console.log(err.response)
-                dispatch(fetchCandidatesFail(err));
-            } );
-    };
+        console.log("This is the converted response", fetchedCandidates);
+        dispatch(fetchCandidatesSuccess(fetchedCandidates));
+      })
+      .catch(err => {
+        console.log(err.response);
+        dispatch(fetchCandidatesFail(err));
+      });
+  };
 };
-
-
-
-
 
 // export const onGetCandidatesHandler = async () => {
 //     return {
